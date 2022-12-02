af72eaa2a5d33ac590579be3a41c4a3d5d27e64e
wik <matt@dreamineering.com>
2022-12-02 12:54:08 +0100

    about page

--- sections/About.jsx
+++ sections/About.jsx
@@ -17,18 +17,27 @@ const About = () => (
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
       <TypingText title="| About Metaversus" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-8 font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold">Metaverse</span> is a new thing in the future, where you
+        can enjoy the virtual world by feeling like it's really real, you can feel what you feel in
+        this metaverse world, because this is really{' '}
+        <span className="font-extrabold">the madness of the metaverse</span> of today, using only{' '}
+        <span className="font-extrabold">VR</span> devices you can easily explore the metaverse
+        world you want, turn your dreams into reality. Let's{' '}
+        <span className="font-extrabold">explore</span> the madness of the metaverse by scrolling
+        down
+      </motion.p>
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      ></motion.img>
     </motion.div>
-    <motion.p
-      variants={fadeIn('up', 'tween', 0.2, 1)}
-      className="mt-8 font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
-    >
-      Metaverse is a new thing in the future, where you can enjoy the virtual world by feeling like
-      it's really real, you can feel what you feel in this metaverse world, because this is really
-      <span className="font-extrabold">the madness of the metaverse</span> of today, using only{' '}
-      <span className="font-extrabold">VR</span> devices you can easily explore the metaverse world
-      you want, turn your dreams into reality. Let's explore the madness of the metaverse by
-      scrolling down
-    </motion.p>
   </section>
 );
 
