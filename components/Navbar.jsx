000ce2ba9771c11cc17334472c5d10298f9884e4
wik <matt@dreamineering.com>
2022-12-02 11:07:37 +0100

    fix html

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,19 +12,18 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className="absolute w-[50%] inset-0 gradient-01">
-      <div
-        className={`${styles.innerWidth} mx-auto flex justify-between gap-8 `}
-      >
-        <img
-          src="/search.svg"
-          alt="search"
-          className="w-[24px] h-[24px] object-contain"
-        />
-        <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-          NFT Market
-        </h2>
-      </div>
+    <div className="absolute w-[50%] inset-0" />
+    <div
+      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+    >
+      <img
+        src="/search.svg"
+        alt="search"
+        className="w-[24px] h-[24px] object-contain"
+      />
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+        NFT Market
+      </h2>
     </div>
   </motion.nav>
 );
